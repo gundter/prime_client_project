6b7fef7863d80c4e661ab9a109d220c7ff102b9d
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-12 10:12:33 -0500

    ticket controller commit

---
+++ client/scripts/controllers/ticketController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by brianaamodt on 6/12/15.
+ */
+
+var App = angular.module('App', []);
+
+App.controller('ticketController', ["$scope", "$http", function($scope, $http){
+        console.log("Controller Empty");
+}]);
