1df7cd0df2c2b2ff616a9406b6088336eeff87f3
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-15 11:46:02 -0500

    brilliant calculator low style commit

--- client/scripts/controllers/calculatorController.js
+++ client/scripts/controllers/calculatorController.js
@@ -6,24 +6,35 @@
 App.controller('calculatorController',['$scope', function($scope){
 //current is the up to date equation as entered by the user
     var current = "";
+//key is set to the last button pressed so that each press is registered and appropriate parameters are met
+    var key = "";
 //scope.equal resolves the  equation and acts "broken" by adding pi to the result
     $scope.equal = function(){
         current = eval(current) + Math.PI;
         $scope.output = current;
+        key = "equal";
     };
 //scope.opBtn inserts an operator into the equation
     $scope.opBtn = function(operator){
         current += operator;
         $scope.output = current;
+        key = operator;
     };
 //scope.numBtn inserts a number into the equation
     $scope.numBtn = function(value){
-        current += value;
-        $scope.output = current;
+        if(key == "equal"){
+            current = value;
+            $scope.output = current;
+        }else{
+            current += value;
+            $scope.output = current;
+            key = value;
+        }
     };
 //scope.clear clears out the equation
     $scope.clear = function(){
         current = "";
         $scope.output = current;
+        key = "";
     };
 }]);
