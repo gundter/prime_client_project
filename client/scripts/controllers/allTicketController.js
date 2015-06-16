419f9c80347e44e8996707936d3d1ad0bc206644
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-16 11:18:19 -0500

    removed redundant calculator.html commit

--- client/scripts/controllers/allTicketController.js
+++ client/scripts/controllers/allTicketController.js
@@ -1,11 +1,11 @@
 App.controller('allTicketController', ['$scope', '$http', function($scope, $http){
 
     $scope.allTickets = [];
+    $scope.ticket = {};
 
     $http.get('/ticket/getTickets').success(
         function(data) {
             console.log("All Tickets", data);
-
             $scope.allTickets = data;
         });
 
