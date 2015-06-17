8fe6c321b653a71d7db9dafc46eab61cd96eb73a
Terry Gunderson <tgunderson25@gmail.com>
2015-06-17 10:11:29 -0500

    adds api.js route, sets up the server side api call routing

--- client/scripts/controllers/registerController.js
+++ client/scripts/controllers/registerController.js
@@ -10,4 +10,21 @@ App.controller('registerController', ['$scope', '$http', function($scope, $http)
         });
     }
 
-}]);
+
+
+}]).directive('pwCheck',function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elem, attrs, ctrl) {
+            var firstPassword = angular.element(document.getElementById(attrs.pwCheck));
+            elem.on('keyup', function () {
+                console.log("Retype password: ",elem.val());
+                scope.$apply(function () {
+                    console.log("first password: ",firstPassword.val());
+                    console.log(elem.val() === firstPassword.val());
+                    ctrl.$setValidity('pwMatch', elem.val() === firstPassword.val());
+                });
+            });
+        }
+    }
+});
