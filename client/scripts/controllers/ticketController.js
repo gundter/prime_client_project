fd3bb66e63df2529104bd4800d5e81c92bedcb88
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-18 14:10:33 -0500

    heroku record button commit

--- client/scripts/controllers/ticketController.js
+++ client/scripts/controllers/ticketController.js
@@ -1,24 +1,35 @@
-App.controller('ticketController', ["$scope", "$http", function($scope, $http){
-        console.log("Controller Empty");
+App.controller('ticketController', ["$scope", "$http", '$sce', function($scope, $http, $sce){
+    console.log("Controller Empty");
 
-        $scope.ticket = {};
-        $scope.user = {};
+    $scope.ticket = {};
+    $scope.user = {};
+    $scope.returnedData = {};
+    $scope.iframe = '';
 
-        $scope.createTicket = function(ticket) {
-                console.log(ticket);
-                $http.post('/ticket/createTicket', ticket).success(
-                    function(data, status, headers, config) {
-                            console.log("Ticket Created ", status);
-                            $scope.ticket = {};
-                    });
-        };
+    $scope.createTicket = function(ticket) {
+        console.log(ticket);
+        $http.post('/ticket/createTicket', ticket).success(
+            function(data, status, headers, config) {
+                console.log("Ticket Created ", status);
+                $scope.ticket = {};
+            });
+    };
 
-        $http.get('/users/user').success(
-            function(data) {
-                    console.log("User response: ", data);
-                    $scope.user = data;
-                    $scope.ticket.email = $scope.user.email;
-                    console.log("Email: ", $scope.user.email);
-            }
-            );
+    $http.get('/users/user').success(
+        function(data) {
+            console.log("User response: ", data);
+            $scope.user = data;
+            $scope.ticket.email = $scope.user.email;
+            console.log("Email: ", $scope.user.email);
+        });
+
+    $http.get('/api/getData').success(
+        function(data) {
+            console.log(data);
+            $scope.returnedData = data;
+            $scope.iframe = $sce.trustAsHtml($scope.returnedData.recordButtonIframe);
+        }).error(
+        function(err) {
+            console.log(err);
+        });
 }]);
