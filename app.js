e7199047073dbb7b18e8068399ddb101e3fa6764
Vince Jones <vincethebutcher@gmail.com>
2015-06-18 16:21:24 -0500

    fixing things

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var API = require('./routes/api');
 var app = express();
 
 // Mongo setup
-var mongoURI = "mongodb://vincethebutcher:winteriscoming9@ds047652.mongolab.com:47652/primeDesk";
+var mongoURI = "mongodb://vincethebutcher:winteriscoming9@ds047652.mongolab.com:47652/primedesk";
 //var mongoURI = 'mongodb://tgun6144:Docix016@ds045242.mongolab.com:45242/primedesk';
 //var mongoURI = "mongodb://localhost:27017/primeDesk";
 
