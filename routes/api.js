a743b2f116859cf37ff71e712c11388901007047
Vince Jones <vincethebutcher@gmail.com>
2015-06-18 16:19:34 -0500

    Add video data to db

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var https = require('https');
 var formUrlEncoded = require('form-urlencoded');
+var VideoData = require('../models/videoDataSchema');
+
 
 router.get('/getData', function(req, res, next){
     var results = new GetData();
@@ -11,10 +13,22 @@ router.get('/getData', function(req, res, next){
     })
 });
 
+//////////////////////////////
+// Save Video Data to database
+//////////////////////////////
 router.post('/', function(req, res, next){
     console.log('Results: ', req.body);
-    res.send(req.body);
+
+    VideoData.create(req.body, function(err, data) {
+       if (err) {
+           console.log("Error with creating videoData: ", err);
+        return next(err);
+       }
+       console.log("Callback Create Video Data: ", data);
+       res.send(data);
+    });
 });
+//////////////////////////////
 
 function GetData(){
     if (!this instanceof GetData){
@@ -26,7 +40,8 @@ var post_data = {
     api_key: 'b6ANvns15XjOUeMbvyzVumTbgJ9stQ',
     api_key_type: 'org_guest_recorder',
     service_name: 'Prime Digital Academy Team',
-    video_endpoint: 'https://dev.ilosvideos.com/embedApiTestEndpoint',
+    // Change video_endpoint for your own website
+    video_endpoint: 'https://polar-reef-6279.herokuapp.com/api',
     video_endpoint_extras: [],
     video_set_public: true
 };
