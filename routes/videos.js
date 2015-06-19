49f1cf4c86394a1fc1d3754910c6be8d6bf002fc
Terry Gunderson <tgunderson25@gmail.com>
2015-06-19 10:55:44 -0500

    adds error checking to videos route

--- routes/videos.js
+++ routes/videos.js
@@ -4,8 +4,9 @@ var Videos = require('../models/videoDataSchema');
 
 router.get('/', function(req, res, next){
     Videos.find(function(err, videos){
+        if (err) return next(err);
         res.json(videos);
-    })
+    });
 });
 
 console.log('videos route loaded');
