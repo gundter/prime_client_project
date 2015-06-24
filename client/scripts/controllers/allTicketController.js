866ddd62e1516552dca8425523a9b865e3733552
Vince Jones <vincethebutcher@gmail.com>
2015-06-24 11:47:45 -0500

    view different tickets

--- client/scripts/controllers/allTicketController.js
+++ client/scripts/controllers/allTicketController.js
@@ -1,6 +1,10 @@
 App.controller('allTicketController', ['$scope', '$http', function($scope, $http){
 
+    $scope.tickets = [];
     $scope.allTickets = [];
+    $scope.archivedTickets = [];
+    $scope.closedTickets = [];
+
     $scope.ticket = {};
     $scope.users = [];
     $scope.userInfo = {};
@@ -15,9 +19,31 @@ App.controller('allTicketController', ['$scope', '$http', function($scope, $http
         $http.get('/ticket/getTickets').then(
             function(response) {
                 console.log("All Tickets", response);
-                $scope.allTickets = response.data;
+
+                for (var i=0; i<response.data; i++) {
+
+                    $scope.allTickets.push(response.data[i]);
+
+                    if (response.data[i].tktStatus == 'tktClosed') {
+                        $scope.closeTickets.push(response.data[i]);
+                    } else if (response.data[i].tktStatus == 'tktArchived') {
+                        $scope.archivedTickets.push(response.data[i]);
+                    }
+                }
             });
     };
+
+    $scope.viewtkt = function(chgTktStatus) {
+            if (chgTktStatus == 'open') {
+                $scope.tickets = $scope.allTickets;
+            } else if (chgTktStatus == 'closed') {
+                $scope.tickets = $scope.closedTickets;
+            } else if (chgTktStatus == 'archived') {
+                $scope.tickets = $scope.archivedTickets;
+            }
+    };
+
+    $scope.viewtkt();
 
     $scope.ticketClass = function(ticket, status){
         console.log("Ticket: ",ticket);
