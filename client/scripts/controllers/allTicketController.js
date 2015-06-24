ae85e032a7ccf562f544b0ee44af57991c68aba4
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-24 11:49:22 -0500

    all ticket hide archive commit

--- client/scripts/controllers/allTicketController.js
+++ client/scripts/controllers/allTicketController.js
@@ -29,5 +29,12 @@ App.controller('allTicketController', ['$scope', '$http', function($scope, $http
         return $http.put('/ticket/updateStatus/', $scope.chgTktSts).success($scope.getTickets);
     };
 
+    $scope.showHide = function(status){
+        $scope.tktArchived.addClass(hide);
+        if (status == 'tktArchived'){
+            $scope.tktArchived.removeClass(hide);
+        }
+    }
+
     $scope.getTickets();
 }]);
