f360498e49750e6f681aaec73465522f7b756902
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-24 10:54:56 -0500

    all ticket style commit

--- client/scripts/controllers/allTicketController.js
+++ client/scripts/controllers/allTicketController.js
@@ -28,17 +28,13 @@ App.controller('allTicketController', ['$scope', '$http', function($scope, $http
         };
         return $http.put('/ticket/updateStatus/', $scope.chgTktSts).success($scope.getTickets);
     };
-    $scope.showHide = function(status){
-        $scope.tktArchived.addClass(hide);
-        if (status == 'tktArchived'){
-            $scope.tktArchived.removeClass(hide);
-        }
-    };
+
     $scope.showHide = function(status) {
         if (status == 'tktArchived') {
             $scope.ticket.tktStatus = 'tktArchive';
-        }else if (status == 'tktArchive') {
-            $scope.ticket.tktStatus = 'tktArchived'}
+        } else if (status == 'tktArchive') {
+            $scope.ticket.tktStatus = 'tktArchived';
+        }
     };
 
     $scope.getTickets();
