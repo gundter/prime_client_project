97911de13b40fd48f4fc5061d021d1ff9a31fbd0
Brian Aamodt <brian.aamodt@gmail.com>
2015-06-25 11:24:01 -0500

    heroku

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -36,5 +36,19 @@ App.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpPro
         otherwise({
             redirectTo: "/home"
         });
-    }]);
+}]);
 
+App.service('browser', ['$window', function($window) {
+
+    return function() {
+        var userAgent = $window.navigator.userAgent;
+        var browsers = {chrome: /chrome/i, safari: /safari/i, firefox: /firefox/i, ie: /internet explorer/i};
+
+        for(var key in browsers) {
+            if (browsers[key].test(userAgent)) {
+                return key;
+            }
+        }
+        return 'unknown';
+    }
+}]);
